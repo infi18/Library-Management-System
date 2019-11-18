1958075391293f63cc80829cdf2636e30b560169
Siddhi Naik <siddhi.naik18@gmail.com>
2019-11-18 13:16:57 -0600

    Add files via upload

---
+++ LibrarySystem.java
@@ -0,0 +1,63 @@
+/**
+ * <h1>Library Management System</h1>
+ *
+ * @author Siddhi Vasudeo Naik
+ * @since 2019-11-16
+ */
+package application;
+
+import controllers.ScreensController;
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * Extends the main Application class and hence load the pages.
+ * Used like a framework
+ *
+ */
+public class LibrarySystem extends Application {
+
+    /**
+     *
+     */
+    public static String screen1ID = "Landing Page";
+    public static String screen1File = "/views/LoginView.fxml";
+    public static String screen2ID = "Client View";
+    public static String screen2File = "/views/ClientView.fxml";
+    public static String screen3ID = "User SignUp";
+    public static String screen3File = "/views/UserSignup.fxml";
+    public static String screen4ID = "Admin View";
+    public static String screen4File = "/views/AdminView.fxml";
+
+    /**
+     * @param primaryStage used to set the main stage and hence
+     * load all the other screens.
+     */
+    @Override
+    public void start(Stage primaryStage) {
+
+        ScreensController mainContainer = new ScreensController();
+        mainContainer.loadScreen(LibrarySystem.screen1ID, LibrarySystem.screen1File);
+        mainContainer.loadScreen(LibrarySystem.screen2ID, LibrarySystem.screen2File);
+        mainContainer.loadScreen(LibrarySystem.screen3ID, LibrarySystem.screen3File);
+        mainContainer.loadScreen(LibrarySystem.screen4ID, LibrarySystem.screen4File);
+        mainContainer.setScreen(LibrarySystem.screen1ID);
+
+        Group root = new Group();
+        root.getChildren().addAll(mainContainer);
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     *
+     * @param args used to launch the fx application.
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
